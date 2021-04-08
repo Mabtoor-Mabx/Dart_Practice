b60fa293835611c11cc15d86a462773892fec068
Mabtoor Mabx <72800208+Mabtoor-Mabx@users.noreply.github.com>
2021-04-08 09:56:55 +0500

    Create Map_Practice.dart
    Done

---
+++ Map_Practice.dart
@@ -0,0 +1,43 @@
+void main()
+{  
+  
+var str1 = {
+    "strname" : "Mabtoor-Ul-Shafiq",
+    "strclass" : "BSCS",
+    "strroll" : "2018-ag-8208"
+};
+
+  
+var str2 = {
+    "strname" : "Horair-Ahmad",
+    "strclass" : "BSCS",
+    "strroll" : "2018-ag-8206"
+};
+ 
+
+var str3 = {
+    "strname" : "Waqas-Sial",
+    "strclass" : "BSCS",
+    "strroll" : "2018-ag-8224"
+};
+  
+  
+  
+var str4 = {
+    "strname" : "Waheed-Ilahi",
+    "strclass" : "BSCS",
+    "strroll" : "2018-ag-8240"
+};
+  
+ 
+var mines = [str1 ,  str2, str3, str4]; 
+ print(mines);   
+  
+  
+  
+  
+  
+  
+  
+  
+}
