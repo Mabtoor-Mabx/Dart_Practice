5c124cc7aad6f763b7a4e811f2f23e66a13754af
Mabtoor Mabx <72800208+Mabtoor-Mabx@users.noreply.github.com>
2021-04-11 23:33:49 +0500

    Create Loops.dart

---
+++ Loops.dart
@@ -0,0 +1,56 @@
+void main()
+{
+  
+  
+//   FOR-LOOP
+//   var n = 3;
+//   for(var i=1;i<=10;i++)
+//   {
+//     print( '${n} X ${i} = ${n*i}');
+//   }
+  
+ 
+  
+  // WHILE_LOOP
+//   var n = 3;
+//   var i = 1;
+//   while(i<=10)
+//   {
+//     print('${n} x ${i} = ${n*i} ');
+//     i++;
+//   }
+  
+  
+  
+  // DO_WHILE_LOOP
+  
+  
+  
+//   var n=9;
+//   var i=1;
+//   do{
+//     print('${n} x ${i} = ${n*i}');
+//     i++;
+//   }while(i<9);
+  
+  
+  
+  
+  // FOR_IN_LOOP
+  
+  
+//   var student = [1,2,3,4,5,6,7];
+//   for(var a in student )
+//   {
+//     print(a);
+//   }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
