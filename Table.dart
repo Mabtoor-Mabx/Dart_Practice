05f646ccca91fc6755766671b0d0baee5610c340
Mabtoor Mabx <72800208+Mabtoor-Mabx@users.noreply.github.com>
2021-04-14 00:14:15 +0500

    Create Table.dart

---
+++ Table.dart
@@ -0,0 +1,22 @@
+void main()
+{
+  
+ 
+  var a = 2;
+  for(var i=1;i<=10;i++)
+  {
+    print("${a} x ${i} = ${a*i} ");
+  }
+  
+  
+  var b = 4;
+  var i=10;
+  while(i>=1)
+  {
+    print("${b} x ${i} = ${b*i} ");
+    i--;
+  }
+  
+  
+  
+}
